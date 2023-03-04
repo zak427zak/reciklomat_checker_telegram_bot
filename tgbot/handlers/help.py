877a7689c9239722c6406aae7052dc91947ff2e0
zak427zak <zak427zak@yandex.ru>
2023-03-04 10:12:35 +0100

    Update help command

--- tgbot/handlers/help.py
+++ tgbot/handlers/help.py
@@ -4,7 +4,7 @@ from aiogram.types import Message
 
 async def user_help(message: Message):
     await message.answer(
-        f"Пожелания, предложения и иная обратная связь - @German_goncharov\n\nP.S. Бот находится в открытом доступе, с радостью приму ваши реквесты здесь https://github.com/zak427zak/reciklomat_checker_telegram_bot",
+        f"Пожелания, предложения и иная обратная связь - @German_goncharov\n\nP.S. Бот находится в открытом доступе, исходный код проекта https://github.com/zak427zak/reciklomat_checker_telegram_bot",
         disable_web_page_preview=True)
 
 
